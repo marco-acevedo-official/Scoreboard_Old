b3abec2977caa987586f9f53535cbd1fa8ecc740
Marco Acevedo <80277847+marco-acevedo-official@users.noreply.github.com>
2024-04-30 19:52:17 -0400

    Update Scoreboard_TB.vhd

--- Scoreboard.srcs/sim_1/new/Scoreboard_TB.vhd
+++ Scoreboard.srcs/sim_1/new/Scoreboard_TB.vhd
@@ -1,81 +1,109 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.numeric_std.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity Scoreboard_TB is
 end Scoreboard_TB;
 
 architecture Simulation of Scoreboard_TB is
-    signal wClk : std_logic := '0';
-    signal wRst : std_logic := '0';
-    signal wInc : std_logic := '0';
-    signal wDec : std_logic := '0';
-    signal wBcd0 : std_logic_vector(3 downto 0) := "0000";
-    signal wBcd1 : std_logic_vector(3 downto 0) := "0000";
-    signal wSd1 : unsigned(6 downto 0) := "0000000";
-    signal wSd0 : unsigned(6 downto 0) := "0000000";
-    constant clk_period : time := 10ns;
-
-    component Scoreboard is
-        port(
+    constant clock_period : time := 10 ns;
+    constant Debouncer_Register_Size : integer := 16;
+    signal clock : std_logic := '0';
+    signal reset_button : std_logic := '0';
+    signal increase_button, inc_deb_to_sync, inc_sync_to_sc : std_logic := '0';
+    signal decrease_button : std_logic := '0';
+    signal Display_0 : unsigned(6 downto 0) := (others => '0');
+    signal Display_1 : unsigned(6 downto 0) := (others => '0');
+    signal BCD_0 : std_logic_vector(3 downto 0) := (others => '0');
+    signal BCD_1 : std_logic_vector(3 downto 0) := (others => '0');
+    
+    component Debouncer is
+    generic(
+        clock_period : time;
+        reg_width : integer
+    );
+    port(
+       clk : in STD_LOGIC;
+       input_signal : in STD_LOGIC;
+       debounced_signal : out STD_LOGIC; -- Debounced output signal
+       reg_out : out STD_LOGIC_VECTOR(reg_width-1 downto 0)
+    );
+    end component;
+    
+    component synchronizer  is
+    generic(
+        clock_period : time
+    );
+    port(
+        rst      : in std_logic;
+        clk      : in std_logic;
+        data_in  : in std_logic;
+        data_out : out std_logic
+    );
+    end component;
+    
+    component  Scoreboard is
+    port(
         clk, rst, inc, dec: in std_logic;
-        bcd1_out, bcd0_out : out std_logic_vector(3 downto 0);--Debug
-        seg7disp1, seg7disp0: out unsigned(6 downto 0)
-        );
+        seg7disp1, seg7disp0: out unsigned(6 downto 0);
+        bcd1_out, bcd0_out : out std_logic_vector(3 downto 0)--Debug
+    );
     end component;
 
-begin
-
-    uut: Scoreboard port map
-    (
-        clk => wClk,
-        rst => wRst,
-        inc => wInc,
-        dec => wDec,
-        bcd1_out => wBcd1,
-        bcd0_out => wBcd0,
-        seg7disp1 => wSd1,
-        seg7disp0 => wSd0
+    begin
+    
+    deb_inc : Debouncer
+    generic map(
+    clock_period => clock_period,
+    reg_width => Debouncer_Register_Size
+    )
+    port map(
+        clk => clock,
+        input_signal => increase_button,
+        debounced_signal => inc_deb_to_sync
     );
-
-    clk_process: process
-    begin                                       
-        wClk <= '0';                            
-        wait for clk_period / 2;                
-        wClk <= '1';                            
-        wait for clk_period / 2;                
-    end process clk_process; 
-
-stimulus: process
-begin
-
-        wRst <= '1';  -- Assert reset
-        wait for clk_period;  -- Wait for a few clock cycles
-        
-        wRst <= '0';  -- De-assert reset
-        wait for clk_period;  -- Wait for a few clock cycles
-
-    -- Loop 9 times
-    for i in 1 to 99 loop
-        -- Toggle inc and dec to simulate some input changes
-        wInc <= '1';
-        wait for clk_period;
-        wInc <= '0';
-        wait for clk_period;
-    end loop;
     
     
-        for i in 1 to 99 loop
-        -- Toggle inc and dec to simulate some input changes
-        wDec <= '1';
-        wait for clk_period;
-        wDec <= '0';
-        wait for clk_period;
-    end loop;
+    sync_inc: Synchronizer 
+    generic map(
+        clock_period => clock_period
+    )
+    port map (
+        rst => reset_button,
+        clk => clock,
+        data_in => inc_deb_to_sync,
+        data_out => inc_sync_to_sc
+    );
     
-    wait;
+    sc : Scoreboard
+    port map(
+        clk => clock,
+        rst => reset_button,
+        inc => inc_sync_to_sc,
+        dec => decrease_button,
+        seg7disp1 => Display_1,
+        seg7disp0 => Display_0,
+        bcd1_out => BCD_1,
+        bcd0_out => BCD_0
+    );
     
-end process stimulus;
-
-
+    process
+    begin
+        while true loop
+            clock <= '1';
+            wait for clock_period / 2;
+            clock <= '0';
+            wait for clock_period / 2;
+        end loop;
+    end process;
+    
+    
+    process
+    begin
+        input_button <= '1';
+        wait for 10 * clock_period;
+        input_button <= '0';
+        wait;
+    end process;
+    
 end architecture Simulation;
